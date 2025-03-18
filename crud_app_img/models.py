7327f283f829123c19b631249a1a197992e2884e
JB985 <bermudezjohan805jm@gmail.com>
2025-03-18 17:58:54 -0500

    se añadio el modelo usuario y el modelo de roles

--- crud_app_img/models.py
+++ crud_app_img/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 def validar_extencion(value):
     import os
@@ -13,7 +14,7 @@ class Item(models.Model):
     description = models.TextField(verbose_name='Descripción')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Precio')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Añadido en')
-    image = models.ImageField(upload_to='productos/img/', verbose_name='Imagen')
+    image = models.ImageField(upload_to='productos/img/', verbose_name='Imagen', validators=[validar_extencion])
     
     def __str__(self):
         return self.name
@@ -31,3 +32,37 @@ class Item(models.Model):
     def delete(self, *args, **kwargs):
         self.image.delete()
         super().delete(*args, **kwargs)
+
+class Rol(models.Model):
+    code = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, verbose_name='Nombre')
+
+class User(AbstractUser):
+    role = models.ForeignKey(Rol, on_delete=models.CASCADE, related_name='Roles')
+    username = models.CharField(max_length=255, unique=True, verbose_name='Nombre de Usuario')
+    email = models.EmailField(verbose_name='Correo Electrónico', unique=True)
+    first_name = models.CharField(max_length=255, verbose_name='Nombre')
+    last_name = models.CharField(max_length=255, verbose_name='Apellido')
+    date_of_birth = models.DateField(verbose_name='Fecha de Nacimiento')
+    password1 = models.CharField(max_length=255, verbose_name='Contraseña')
+    password2 = models.CharField(max_length=255, verbose_name='Confirmar Contraseña')
+    phone_number = models.CharField(max_length=255, verbose_name='Número de Teléfono')
+    address = models.CharField(max_length=255, verbose_name='Dirección')
+    image = models.ImageField(upload_to='usuarios/img/', verbose_name='Imagen', blank=True, null=True, validators=[validar_extencion])
+    
+    def save(self, *args, **kwargs):
+        if self.pk:
+            try:
+                old_image = User.objects.get(pk=self.pk)
+                if old_image.image != self.image:
+                    old_image.image.delete(save=False)
+            except Item.DoesNotExist:
+                pass 
+        super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super().delete(*args, **kwargs)
+    
+    def __str__(self):
+        return self.username
--- crud_img_efectivo/settings.py
+++ crud_img_efectivo/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+AUTH_USER_MODEL = 'crud_app_img.User'
 
 
 # Quick-start development settings - unsuitable for production
