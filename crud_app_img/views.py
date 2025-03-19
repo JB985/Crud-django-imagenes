36598f66ab89c12e2a2b219cccc3fd2ae35ef2a7
JB985 <bermudezjohan805jm@gmail.com>
2025-03-19 12:09:37 -0500

    se añadio una barra de busqueda

--- crud_app_img/views.py
+++ crud_app_img/views.py
@@ -3,8 +3,12 @@ from .models import Item
 from .forms import ItemForm
 
 def item_list(request):
-    items = Item.objects.all()
-    return render(request, 'app/item_list.html', {'items': items})
+    query = request.GET.get('search')
+    if query:
+        items = Item.objects.filter(name__icontains=query)
+    else:
+        items = Item.objects.all()
+    return render(request, 'app/item_list.html', {'items': items, 'query': query})
 
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
