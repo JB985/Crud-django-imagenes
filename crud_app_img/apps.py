e0ca52533ac4148aa68da26c1bd51885c0f0d875
JB985 <bermudezjohan805jm@gmail.com>
2025-03-24 10:26:01 -0500

    Se aañadido un sistema de reinicio del id.

--- crud_app_img/apps.py
+++ crud_app_img/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class CrudAppImgConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crud_app_img'
+
+    def ready(self):
+        from . import signals
--- crud_img_efectivo/settings.py
+++ crud_img_efectivo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crud_app_img.apps.CrudAppImgConfig',
     'crud_app_img'
 ]
 
